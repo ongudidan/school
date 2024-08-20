a0d81a93dda67bf3815dd8265fbf68ded3c2b670
ongudidan <ngudidan@gmail.com>
2024-08-20 21:19:53 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.php
    	modified:   runtime/debug/index.data

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, "yii2.cfg");
 $dotenv->safeLoad();
-if (isset($_SERVER['ENVIRONMENT']) && $_SERVER['ENVIRONMENT'] == 'dev') {
-    defined('YII_DEBUG') or define('YII_DEBUG', true);
-    defined('YII_ENV') or define('YII_ENV', 'dev');
+if (isset($_SERVER['ENVIRONMENT']) && $_SERVER['ENVIRONMENT'] == 'prod') {
+    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_ENV') or define('YII_ENV', 'prod');
 }
 require __DIR__ . '/vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__ . '/config/web.php';
